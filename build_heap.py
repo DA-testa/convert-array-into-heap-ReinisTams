17bac32ef1ffb23b7ffa4a1a58301555ce3ab62e
ReinisTams <125902838+ReinisTams@users.noreply.github.com>
2023-06-01 15:45:53 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,52 +1,65 @@
 # python3
 
 def build_heap(data):
-    swaps = []
-    n = len(data)
-    for i in range(n // 2 - 1, -1, -1):
-        sift_down(data, i, n, swaps)
+    #Saraksts, kur glabāt apmaiņas operācijas
+    swaps = []  
+    #Iegūst datu saraksta elementu skaitu
+    n = len(data)  
+    for i in range(n // 2 - 1, -1, -1):  #Ciklē no pēdējā vecākā mezgla līdz pirmajam (no augšas uz leju)
+        sift_down(data, i, n, swaps)  # Izsauc funkciju sift_down, lai saglabātu koku
 
     return swaps
 
-
 def sift_down(data, i, n, swaps):
-    index = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+    #Pieņem, ka pašreizējais mezgls ir vismazākais
+    index = i  
+    #Aprēķina kreisā bērna mezgla indeksu
+    left = 2 * i + 1  
+    #Aprēķina labā bērna mezgla indeksu
+    right = 2 * i + 2  
 
+    # Salīdzina kreiso bērnu ar pašreizējo vismazāko mezglu
     if left < n and data[left] < data[index]:
         index = left
+
+    # Salīdzina labo bērnu ar pašreizējo vismazāko mezglu
     if right < n and data[right] < data[index]:
         index = right
+
+    # Ja pašreizējais mezgls nav vismazākais, to nomaina ar vismazāko bērnu
     if i != index:
-        data[i], data[index] = data[index], data[i]
-        swaps.append((i, index))
-        sift_down(data, index, n, swaps)
+        data[i], data[index] = data[index], data[i]  # Nomaina mezglus vietām
+        swaps.append((i, index))  # Reģistrē apmaiņas operāciju
+        sift_down(data, index, n, swaps)  # Rekursīvi turpina apstrādāt apmainīto mezglu
 
 
 def main():
-    choice = input("Enter 'I' for interactive input or 'F' for file input: ")
-    if choice == "I":
-        n = int(input("Enter the number of elements: "))
-        data = list(map(int, input("Enter the elements separated by spaces: ").split()))
-        assert len(data) == n
-    elif choice == "F":
-        file_path = input("Enter the file path: ")
-        with open(file_path, 'r') as file:
-            n = int(file.readline())
-            data = list(map(int, file.readline().split()))
-            assert len(data) == n
+    choice = input("Ievadiet 'I', lai ievadītu datus interaktīvi, vai 'F', lai ievadītu datus no faila: ")
+    if choice == "I":  #ievade
+        n = int(input("Ievadiet elementu skaitu: "))  #Ievada elementu skaitu
+        data = list(map(int, input("Ievadiet elementus, atdalot tos ar atstarpēm: ").split()))  #Ievada elementus
+        assert len(data) == n  #Pārbauda ka elementu skaits atbilst norādītajam
+    elif choice == "F":  #Faila ievade
+        file_path = input("Ievadiet faila ceļu: ")  #Ievada faila ceļu
+        
+        with open(file_path, 'r') as file:  #Atver failu
+            n = int(file.readline())  #Nolasa elementu skaitu no pirmās rindas
+            data = list(map(int, file.readline().split()))  #Nolasa elementus no otrās rindas
+            assert len(data) == n  #Pārliecinās, ka elementu skaits atbilst norādītajam
     else:
-        print("Invalid input. Please enter 'I' or 'F'.")
+        print("Nederīga ievade. Lūdzu, ievadiet 'I' vai 'F'.")
         return
 
-    swaps = build_heap(data)
-    assert len(swaps) <= 4 * n
-    print("Number of swaps:", len(swaps))
+    swaps = build_heap(data)  #Veido three un iegūst apmaiņas operācijas
+    assert len(swaps) <= 4 * n  #Pārliecinās ka apmaiņas operāciju skaits ir pareizā ierobežojumā
+    print("Apmaiņas operāciju skaits:", len(swaps))  #Izdrukā veiktās apmaiņas operācijas skaitu
     for i, j in swaps:
-        print(i, j)
+        
+        print(i, j)  #Izdrukā apmainīto elementu indeksus
 
 
 if __name__ == "__main__":
+    main()
+== "__main__":
     main()
 
